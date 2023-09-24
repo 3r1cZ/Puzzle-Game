2eb5afee247f1fbc79462bcde75ff98003772259
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-09-24 18:13:06 -0400

    Implement image and caption display for imageGuesser

--- Puzzles/ImageGuesser/script-imageGuesser.js
+++ Puzzles/ImageGuesser/script-imageGuesser.js
@@ -1,6 +1,10 @@
+let randomImageNum;
+
 function loadImageGuesser() {
     getText('imageLinks.txt')
-    .then(text => getImage(text));
+    .then(image => getImage(image));
+    getText('imageText.txt')
+    .then(displayCaption);
 }
 
 // read files
@@ -10,7 +14,21 @@ async function getText(file) {
     return text;
 }
 
-function getImage(text) {
-    
-    document.getElementById("image").src = "Images/big-mac.jpg";
+function getImage(image) {
+    let imageArr = image.split("\n"); // convert text string to array
+    randomImageNum = Math.floor(Math.random() * imageArr.length);
+    let randomImageLink = imageArr[randomImageNum];
+    document.getElementById("image").src = randomImageLink;
+}
+
+function displayCaption(text) {
+    let textArr = text.split("\n");
+    let captionText = textArr[randomImageNum];
+    captionText = captionText.replace(/[\r\n]+/gm, "");
+    let numLetters = captionText.length;
+    let underlines = '';
+    for(let i=0; i<numLetters; i++){
+        underlines = underlines.concat(" _");
+    }
+    document.getElementById("image-text").textContent = underlines;
 }
