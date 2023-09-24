7b3e83f8de316cb7249dbb24874d51450fa87406
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-09-24 19:22:10 -0400

    Implement automatic movement between input fields

--- Puzzles/ImageGuesser/script-imageGuesser.js
+++ Puzzles/ImageGuesser/script-imageGuesser.js
@@ -14,6 +14,7 @@ async function getText(file) {
     return text;
 }
 
+// displays image
 function getImage(image) {
     let imageArr = image.split("\n"); // convert text string to array
     randomImageNum = Math.floor(Math.random() * imageArr.length);
@@ -21,6 +22,7 @@ function getImage(image) {
     document.getElementById("image").src = randomImageLink;
 }
 
+// displays underlines for caption of images
 function displayCaption(text) {
     let textArr = text.split("\n");
     let captionText = textArr[randomImageNum];
@@ -32,7 +34,21 @@ function displayCaption(text) {
         input.setAttribute('maxlength', 1);
         input.setAttribute('placeholder', '_');
         input.setAttribute('id', 'image-text');
-        const captionDiv = document.getElementById("image-text-container");
-        captionDiv.appendChild(input);
+        const captionDiv = document.getElementById("image-text-container"); 
+        captionDiv.appendChild(input); // add input field to caption div
+    }
+    moveInput();
+}
+
+// moves to next input field when an input is entered
+function moveInput(){
+    let captionDiv = document.getElementById("image-text-container"); 
+    for(let field of captionDiv.children) {
+        field.addEventListener('input', function(e) {
+            //input is not backspace and there is another input field -> go to the next field
+            if (e.inputType != "deleteContentBackward" && field.nextElementSibling) {
+                field.nextElementSibling.focus();
+            }
+        });
     }
 }
