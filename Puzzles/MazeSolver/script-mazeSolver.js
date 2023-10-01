c1822b43d0a5313b9092d34e9cae236c8c6d4747
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-10-01 14:04:16 -0400

    Change maze drawing to canvas

--- Puzzles/MazeSolver/script-mazeSolver.js
+++ Puzzles/MazeSolver/script-mazeSolver.js
@@ -3,12 +3,6 @@ function loadMazeSolver(){
 }
 
 function drawWalls(){
-    const maze = document.getElementById("maze");
-    let walls = [];
-    for(let i=0; i<20; i++){
-        const square = document.createElement("div");
-        square.setAttribute('class', 'wall');
-        maze.appendChild(square);
-        walls.push(square);
-    }
+    const canvas = document.getElementById("canvas");
+    
 }
